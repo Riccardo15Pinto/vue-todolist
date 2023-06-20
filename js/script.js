204715c8ee165d4c502741d5735d934b04ea8a8e
Riccardo15Pinto <riccardopinto59@gmail.com>
2023-06-20 16:39:37 +0200

    add comment

--- js/script.js
+++ js/script.js
@@ -2,10 +2,14 @@
 console.log('ok js');
 console.log('ok vue', Vue);
 
+//create app
 const app = Vue.createApp({
+    //data
     data(){
         return{
+            //element to push in array
             newGoals: '',
+            // array goals
             listGoals :[
             {
                 text : 'Imposta la sveglia',
@@ -38,16 +42,17 @@ const app = Vue.createApp({
             ]
         }
     },
-
+    //methods
     methods:{
+        //change/toogle listGoals.done to false/true
         isTrue(target){
             this.listGoals[target].done = !this.listGoals[target].done
         },  
-
+        //delete goals from array
         deleteGoals(index){
            this.listGoals.splice(index, 1);
         },
-
+        //push newGoals in the array
         addnewGoals(){
             if(this.newGoals.length > 0){
                 this.listGoals.push({text : this.newGoals , done: false});
@@ -58,5 +63,5 @@ const app = Vue.createApp({
 
     }
 })
-
+// print in html
 app.mount('#root');
